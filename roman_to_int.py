31191e814df64072d1862cadef28791bb83953c6
Matt Yeung <mqyeung@gmail.com>
2023-04-21 23:53:12 -0700

    Create roman_to_int.py

---
+++ roman_to_int.py
@@ -0,0 +1,23 @@
+def roman_to_int(s: str) -> int:
+    roman_map = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    result = 0
+    prev_value = 0
+
+    for i in range(len(s) - 1, -1, -1):
+        value = roman_map[s[i]]
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+        prev_value = value
+
+    return result
